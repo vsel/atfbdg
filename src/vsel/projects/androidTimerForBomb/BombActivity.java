bfc08e6861007a1bd7cd9d4ac1c2c71b6888c592
vsel <vselyukh@gmail.com>
2014-03-16 14:07:01 +0200

    Remove useless sounds, change animation time, remove some bugs, no lanscape mode made

--- src/vsel/projects/androidTimerForBomb/BombActivity.java
+++ src/vsel/projects/androidTimerForBomb/BombActivity.java
@@ -15,81 +15,108 @@ import com.vsel.atfbdg.R;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
-import android.media.AudioManager;
-import android.media.SoundPool;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
 public class BombActivity extends Activity {
 	private AnimationDrawable bombAnim;
-	// Sound variables
-
-	// AudioManager
-	private AudioManager mAudioManager;
-	// SoundPool
-	private SoundPool mSoundPool;
-	// ID for the bubble popping sound
-	private int mSoundID;
-	// Audio volume
-	private float mStreamVolume;
-	//TODO - Add gesture. Start on untap.
+	//Media Player
+	MediaPlayer mpintro,mpoutro;
+	// Gesture Detector
+	private GestureDetector mGestureDetector;
+	//TODO - don`t rerun in portrait mode. Block portrait mode.
+	private void setupGestureDetector() {
 	
-	private void log(String msg) {
-		Log.i("AAA", msg);
-	}
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-
-		ImageView imageView = (ImageView) findViewById(R.id.countdown_frame);
+	mGestureDetector = new GestureDetector(this,
+	
+		new GestureDetector.SimpleOnGestureListener() {
 		
-		imageView.setBackgroundResource(R.drawable.view_animation);
-
-		bombAnim = (AnimationDrawable) imageView.getBackground();
-		mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-
-		mStreamVolume = (float) mAudioManager
-				.getStreamVolume(AudioManager.STREAM_MUSIC)
-				/ mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-
-        //new SoundPool, allowing up to 10 streams
-        mSoundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-	    mSoundID = mSoundPool.load(getApplicationContext(), R.raw.bah, 1);
-
-
-	}
-	protected void onPause() {
- 		super.onPause();
-
-	}
-
-
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus) {
-		super.onWindowFocusChanged(hasFocus);
-		Random r=new Random();
-		int bombTimer=(r.nextInt(50)+10);
-		if (hasFocus) {
+		// If a fling gesture starts on a BubbleView then change the
+		// BubbleView's velocity
+		
+		@Override
+		public boolean onSingleTapConfirmed(MotionEvent event) {
+			//if(event.ACTION_POINTER_UP(event.));
+			Random r=new Random();
+			int bombTimer=(r.nextInt(50)+10);
+			mpintro.setLooping(true);
+			mpintro.start();
 			bombAnim.start();
 			// TODO - Add start of tick-tack
 			Timer timer = new Timer();
 		    MyTimerTask myTimerTask = new MyTimerTask();
 		    timer.schedule(myTimerTask, bombTimer*1000);
 		    //log("test"+bombTimer*100);
+		    return false;
 		}
+	});
 	}
-
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO - delegate the touch to the gestureDetector 
+		return mGestureDetector.onTouchEvent(event);
+	}
+	private void log(String msg) {
+		Log.i("AAA", msg);
+	}
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		ImageView imageView = (ImageView) findViewById(R.id.countdown_frame);
+		imageView.setBackgroundResource(R.drawable.view_animation);
+		
+		bombAnim = (AnimationDrawable) imageView.getBackground();
+		
+		mpintro = MediaPlayer.create(this, R.raw.tictak);
+		mpoutro = MediaPlayer.create(this, R.raw.bah);
+		setupGestureDetector();
+		
+	}
+	protected void onPause() {
+		// TODO - Release all SoundPool resources
+		super.onPause();
+		mpintro.reset();
+		mpintro=null;
+		mpoutro.reset();
+		mpoutro=null;
+	}
+	public void onResume(Bundle savedInstanceState) {
+		super.onResume();
+		//setupGestureDetector();
+	}
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		/*
+		//Because of getting to gesture
+		Random r=new Random();
+		int bombTimer=(r.nextInt(50)+10);
+		if (hasFocus) {
+		//mpintro.setLooping(true);
+		       mpintro.start();
+		bombAnim.start();
+		// TODO - Add start of tick-tack
+		Timer timer = new Timer();
+		   MyTimerTask myTimerTask = new MyTimerTask();
+		   timer.schedule(myTimerTask, bombTimer*1000);
+		   //log("test"+bombTimer*100);
+		    * 
+		    */
+	}
+	
 	class MyTimerTask extends TimerTask {
-
-		  @Override
-		  public void run() {
-			  bombAnim.stop();
-			  //log("working");
-			  // TODO - Add stop of tick-tack
-		      mSoundPool.play(mSoundID, mStreamVolume, mStreamVolume, 100, 0, 1f);
-		  }	
-		  
-
+	
+	 @Override
+	 public void run() {
+		 bombAnim.stop();
+		 mpintro.pause();
+		 mpoutro.start();	
+	 }	
+	
 	}
 }
