230cf8baf6d049cc9bebf618514f42f168f8d57c
vsel <vsel@users.noreply.github.com>
2014-05-13 12:19:08 +0300

    Update BombActivity.java

--- src/vsel/projects/androidTimerForBomb/BombActivity.java
+++ src/vsel/projects/androidTimerForBomb/BombActivity.java
@@ -17,6 +17,8 @@ import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -29,6 +31,26 @@ public class BombActivity extends Activity {
 	// Gesture Detector
 	private GestureDetector mGestureDetector;
 	//TODO - don`t rerun in portrait mode. Block portrait mode.
+	
+	private static final int DISPLAY_DATA = 1;
+    // this handler will receive a delayed message
+    private Handler mHandler = new Handler() {
+        @Override
+		public void handleMessage(Message msg) {
+            // Do task here
+            if (msg.what == DISPLAY_DATA){
+            	ImageView imageView = (ImageView) findViewById(R.id.countdown_frame);
+	        	imageView.setBackgroundResource(R.drawable.view_animation2);
+	        	bombAnim = (AnimationDrawable) imageView.getBackground();
+	        	//bombAnim.setVisible(false, false);
+	                //bombAnim.stop();
+	    		mpintro.pause();
+	    		mpoutro.start();	
+            }
+        }
+	
+    };
+        
 	private void setupGestureDetector() {
 	
 	mGestureDetector = new GestureDetector(this,
@@ -42,11 +64,11 @@ public class BombActivity extends Activity {
 			int bombTimer=(r.nextInt(50)+10);
 			mpintro.setLooping(true);
 			mpintro.start();
+			ImageView imageView = (ImageView) findViewById(R.id.countdown_frame);
+			imageView.setBackgroundResource(R.drawable.view_animation);
+			bombAnim = (AnimationDrawable) imageView.getBackground();
 			bombAnim.start();
-			Timer timer = new Timer();
-		    MyTimerTask myTimerTask = new MyTimerTask();
-		    timer.schedule(myTimerTask, bombTimer*1000);
-		    //log("test"+bombTimer*100);
+			mHandler.sendEmptyMessageDelayed(DISPLAY_DATA, bombTimer*1000);
 		    return false;
 		}
 	});
@@ -104,14 +126,4 @@ public class BombActivity extends Activity {
 		    */
 	}
 	
-	class MyTimerTask extends TimerTask {
-	
-	 @Override
-	 public void run() {
-		 bombAnim.stop();
-		 mpintro.pause();
-		 mpoutro.start();	
-	 }	
-	
-	}
 }
