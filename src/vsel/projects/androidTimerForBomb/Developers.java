047abc1eea5f580ec12e2e8fb52d88333889eaf0
vselpublic <volodymyr.volodymyrovych@gmail.com>
2014-06-27 15:35:36 +0300

    Create Developers.java
    Add developers page code

---
+++ src/vsel/projects/androidTimerForBomb/Developers.java
@@ -0,0 +1,42 @@
+/**
+ * @author vsel
+ *
+ */
+
+package vsel.projects.androidTimerForBomb;
+
+import com.vsel.atfbdg.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Developers extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.developers);		
+	}
+	
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+}
