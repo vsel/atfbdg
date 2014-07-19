2b2364629ea038720a589efcda97fb5b03fdc81e
vsel <vselyukh@gmail.com>
2014-07-19 22:47:11 +0300

    AdMob added

--- src/vsel/projects/androidTimerForBomb/BombActivity.java
+++ src/vsel/projects/androidTimerForBomb/BombActivity.java
@@ -6,6 +6,10 @@
 package vsel.projects.androidTimerForBomb;
 
 import java.util.Random;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import com.vsel.atfbdg.R;
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
@@ -17,8 +21,11 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class BombActivity extends Activity {
+	private AdView adView;
+	
 	private AnimationDrawable bombAnim;
 	//Media Player
 	MediaPlayer mpintro,mpoutro;
@@ -79,6 +86,26 @@ public class BombActivity extends Activity {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.main);
 			
+			// Create an ad.
+		    adView = new AdView(this);
+		    adView.setAdSize(AdSize.BANNER);
+		    adView.setAdUnitId("ca-app-pub-8434465505627354/3980880420");
+
+		    // Add the AdView to the view hierarchy. The view will have no size
+		    // until the ad is loaded.
+		    LinearLayout layout = (LinearLayout) findViewById(R.id.frame);
+		    layout.addView(adView);
+
+		    // Create an ad request. Check logcat output for the hashed device ID to
+		    // get test ads on a physical device.
+		    AdRequest adRequest = new AdRequest.Builder()
+		        //.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+		        //.addTestDevice("INSERT_YOUR_HASHED_DEVICE_ID_HERE")
+		        .build();
+
+		    // Start loading the ad in the background.
+		    adView.loadAd(adRequest);
+			
 			ImageView imageView = (ImageView) findViewById(R.id.countdown_frame);
 			imageView.setBackgroundResource(R.drawable.view_animation);
 			
@@ -91,11 +118,11 @@ public class BombActivity extends Activity {
 		}
 		protected void onPause() {
 			//Release all SoundPool resources
+			super.onPause();
 			mpintro.release();
 			mpintro=null;
 			mpoutro.release();
 			mpoutro=null;
-			super.onPause();
 		}
 		public void onResume(Bundle savedInstanceState) {
 			super.onResume();
@@ -109,11 +136,11 @@ public class BombActivity extends Activity {
 		@Override
 		protected void onStop() {
 			//Release all SoundPool resources
+			super.onStop();
 			mpintro.release();
 			mpintro=null;
 			mpoutro.release();
 			mpoutro=null;
-			super.onStop();
 		}
 
 		@Override
--- src/vsel/projects/androidTimerForBomb/Developers.java
+++ src/vsel/projects/androidTimerForBomb/Developers.java
@@ -5,38 +5,62 @@
 
 package vsel.projects.androidTimerForBomb;
 
+import com.google.android.gms.ads.*;
 import com.vsel.atfbdg.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Developers extends Activity {
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.developers);		
-	}
+	private AdView adView;
 	
 	@Override
-	protected void onRestart() {
-		super.onRestart();
-	}
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.developers);
+		
+		// Create an ad.
+	    adView = new AdView(this);
+	    adView.setAdSize(AdSize.BANNER);
+	    adView.setAdUnitId("ca-app-pub-8434465505627354/3980880420");
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
+	    // Add the AdView to the view hierarchy. The view will have no size
+	    // until the ad is loaded.
+	    LinearLayout layout = (LinearLayout) findViewById(R.id.frame);
+	    layout.addView(adView);
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-	}
+	    // Create an ad request. Check logcat output for the hashed device ID to
+	    // get test ads on a physical device.
+	    AdRequest adRequest = new AdRequest.Builder()
+	        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+	        .addTestDevice("INSERT_YOUR_HASHED_DEVICE_ID_HERE")
+	        .build();
 
-	@Override
-	protected void onStop() {
+	    // Start loading the ad in the background.
+	    adView.loadAd(adRequest);
+	  }
+
+	  @Override
+	  public void onPause() {
+	    adView.pause();
+	    super.onPause();
+	  }
+
+	  @Override
+	  public void onResume() {
+	    super.onResume();
+	    adView.resume();
+	  }
+
+	  @Override
+	  public void onDestroy() {
+	    adView.destroy();
+	    super.onDestroy();
+	  }
+	
+	  @Override
+	  protected void onStop() {
 		super.onStop();
-	}
+	  }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-	}
 }
